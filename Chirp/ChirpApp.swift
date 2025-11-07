25041327d8e0c8bda90fe5c44caeea29292cc298
Connor <82681679+Jc-hammond@users.noreply.github.com>
2025-11-06 19:05:11 -0700

    Initial Commit

---
+++ Chirp/ChirpApp.swift
@@ -0,0 +1,32 @@
+//
+//  ChirpApp.swift
+//  Chirp
+//
+//  Created by Connor Hammond on 11/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ChirpApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
